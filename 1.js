b645d845852e4a774f3f36784b5a9841cdc23be2
jiaxin <jiaxin@qq.com>
2016-10-04 23:05:18 +0800

    左侧页面top部分和中间图片文字切换部分完成

--- 1.js
+++ 1.js
@@ -63,8 +63,172 @@
       }
       my_voice_up.style.width = my_music.volume * 100 + "%";
   }
-
-
+     //听相似歌曲
+  var my_sound_like = document.getElementById("sound_like");
+  var my_sound = document.getElementById("sound");
+  my_sound_like.onmouseover = function(){
+      my_sound.src = "image/sound1.png";
+  }
+  my_sound_like.onmouseout = function(){
+      my_sound.src = "image/sound.png";
+  }
+      //来下载吧
+  var my_xiazai = document.getElementById("xiazai");
+  var my_xiazaiba = document.getElementById("xiazaiba");
+  my_xiazai.onmouseover = function(){
+      my_xiazaiba.src = "image/xiazai1.png";
+  }
+  my_xiazai.onmouseout = function(){
+      my_xiazaiba.src = "image/xiazai.png";
+  }
+      //左侧标志
+  var my_chuxian = document.getElementById("chuxian");
+  my_chuxian.onmouseover = function () {
+    my_chuxian.src = "image/chuxian1.png";
+  }
+  my_chuxian.onmouseout = function () {
+    my_chuxian.src = "image/chuxian.png";
+  }
+  //鼠标放在圆圈上变换图片和文字
+  var my_tupian = document.getElementById("tupian1");
+  var yuan1 = document.getElementById("yuan1");
+  var yuan2 = document.getElementById("yuan2");
+  var yuan3 = document.getElementById("yuan3");
+  var yuan4 = document.getElementById("yuan4");
+  var yuan5 = document.getElementById("yuan5");
+  var yuan6 = document.getElementById("yuan6");
+  var song_name = document.getElementById("song_name");
+  var song_shuoming = document.getElementById("song_shuoming");
+  window.setInterval(qiehuan, 5000);
+  function qiehuan() {
+    if(my_tupian.src.match("image/sing.png")){
+      my_tupian.src = "image/girl.png";
+      song_name.innerHTML = "さよならの夏 ～コクリコ坂から～ <br>(主題歌 別バージョン) 系MHz";
+      song_shuoming.innerHTML = "为你推荐与 手嶌葵 - さよならの夏 ～コクリコ坂から～ <br>(主題歌 別バージョン) 相似的歌曲";
+      yuan2.style.backgroundColor = "#9DD6C5";
+      yuan1.style.backgroundColor = "#D4E1DC";
+      yuan3.style.backgroundColor = "#D4E1DC";
+      yuan4.style.backgroundColor = "#D4E1DC";
+      yuan5.style.backgroundColor = "#D4E1DC";
+      yuan6.style.backgroundColor = "#D4E1DC";
+    }else if (my_tupian.src.match("image/girl.png")) {
+      my_tupian.src = "image/sexy.png";
+      song_name.innerHTML = "She 系MHz";
+      song_shuoming.innerHTML = "为你推荐与 Groove Coverage - She 相似的歌曲";
+      yuan3.style.backgroundColor = "#9DD6C5";
+      yuan1.style.backgroundColor = "#D4E1DC";
+      yuan2.style.backgroundColor = "#D4E1DC";
+      yuan4.style.backgroundColor = "#D4E1DC";
+      yuan5.style.backgroundColor = "#D4E1DC";
+      yuan6.style.backgroundColor = "#D4E1DC";
+    }else if (my_tupian.src.match("image/sexy.png")) {
+      my_tupian.src = "image/nanshan.png";
+      song_name.innerHTML = "南山南 系MHz";
+      song_shuoming.innerHTML = "为你推荐与 马頔 - 南山南 相似的歌曲";
+      yuan4.style.backgroundColor = "#9DD6C5";
+      yuan1.style.backgroundColor = "#D4E1DC";
+      yuan3.style.backgroundColor = "#D4E1DC";
+      yuan2.style.backgroundColor = "#D4E1DC";
+      yuan5.style.backgroundColor = "#D4E1DC";
+      yuan6.style.backgroundColor = "#D4E1DC";
+    }else if (my_tupian.src.match("image/nanshan.png")) {
+      my_tupian.src = "image/piano.png";
+      song_name.innerHTML = "映画『天空の城ラピュタ』：<br>innocent 系MHz";
+      song_shuoming.innerHTML = "为你推荐与 久石譲 - 映画『天空の城ラピュタ』：<br>innocent 相似的歌曲";
+      yuan5.style.backgroundColor = "#9DD6C5";
+      yuan1.style.backgroundColor = "#D4E1DC";
+      yuan3.style.backgroundColor = "#D4E1DC";
+      yuan4.style.backgroundColor = "#D4E1DC";
+      yuan2.style.backgroundColor = "#D4E1DC";
+      yuan6.style.backgroundColor = "#D4E1DC";
+    }else if (my_tupian.src.match("image/piano.png")) {
+      my_tupian.src = "image/cute.png";
+      song_name.innerHTML = "SK-II 最初的梦MHz";
+      song_shuoming.innerHTML = "热门歌曲：夜空中最亮的星 / 最初的梦想（Cover 范玮<br>琪） / 一万次悲伤50首歌曲";
+      yuan6.style.backgroundColor = "#9DD6C5";
+      yuan1.style.backgroundColor = "#D4E1DC";
+      yuan3.style.backgroundColor = "#D4E1DC";
+      yuan4.style.backgroundColor = "#D4E1DC";
+      yuan5.style.backgroundColor = "#D4E1DC";
+      yuan2.style.backgroundColor = "#D4E1DC";
+    }else {
+      my_tupian.src = "image/sing.png";
+      song_name.innerHTML = "Love the Way You Lie 系MHz";
+      song_shuoming.innerHTML = "为你推荐与 Skylar Grey - Love the Way You Lie 相似<br>的歌曲";
+      yuan1.style.backgroundColor = "#9DD6C5";
+      yuan2.style.backgroundColor = "#D4E1DC";
+      yuan3.style.backgroundColor = "#D4E1DC";
+      yuan4.style.backgroundColor = "#D4E1DC";
+      yuan5.style.backgroundColor = "#D4E1DC";
+      yuan6.style.backgroundColor = "#D4E1DC";
+    }
+  }
+  yuan1.onmouseover = function(){
+      my_tupian.src = "image/sing.png";
+      song_name.innerHTML = "Love the Way You Lie 系MHz";
+      song_shuoming.innerHTML = "为你推荐与 Skylar Grey - Love the Way You Lie 相似<br>的歌曲";
+      yuan1.style.backgroundColor = "#9DD6C5";
+      yuan2.style.backgroundColor = "#D4E1DC";
+      yuan3.style.backgroundColor = "#D4E1DC";
+      yuan4.style.backgroundColor = "#D4E1DC";
+      yuan5.style.backgroundColor = "#D4E1DC";
+      yuan6.style.backgroundColor = "#D4E1DC";
+  }
+  yuan2.onmouseover = function(){
+      my_tupian.src = "image/girl.png";
+      song_name.innerHTML = "さよならの夏 ～コクリコ坂から～ <br>(主題歌 別バージョン) 系MHz";
+      song_shuoming.innerHTML = "为你推荐与 手嶌葵 - さよならの夏 ～コクリコ坂から～ <br>(主題歌 別バージョン) 相似的歌曲";
+      yuan2.style.backgroundColor = "#9DD6C5";
+      yuan1.style.backgroundColor = "#D4E1DC";
+      yuan3.style.backgroundColor = "#D4E1DC";
+      yuan4.style.backgroundColor = "#D4E1DC";
+      yuan5.style.backgroundColor = "#D4E1DC";
+      yuan6.style.backgroundColor = "#D4E1DC";
+  }
+  yuan3.onmouseover = function(){
+      my_tupian.src = "image/sexy.png";
+      song_name.innerHTML = "She 系MHz";
+      song_shuoming.innerHTML = "为你推荐与 Groove Coverage - She 相似的歌曲";
+      yuan3.style.backgroundColor = "#9DD6C5";
+      yuan1.style.backgroundColor = "#D4E1DC";
+      yuan2.style.backgroundColor = "#D4E1DC";
+      yuan4.style.backgroundColor = "#D4E1DC";
+      yuan5.style.backgroundColor = "#D4E1DC";
+      yuan6.style.backgroundColor = "#D4E1DC";
+  }
+  yuan4.onmouseover = function(){
+      my_tupian.src = "image/nanshan.png";
+      song_name.innerHTML = "南山南 系MHz";
+      song_shuoming.innerHTML = "为你推荐与 马頔 - 南山南 相似的歌曲";
+      yuan4.style.backgroundColor = "#9DD6C5";
+      yuan1.style.backgroundColor = "#D4E1DC";
+      yuan3.style.backgroundColor = "#D4E1DC";
+      yuan2.style.backgroundColor = "#D4E1DC";
+      yuan5.style.backgroundColor = "#D4E1DC";
+      yuan6.style.backgroundColor = "#D4E1DC";
+  }
+  yuan5.onmouseover = function(){
+      my_tupian.src = "image/piano.png";
+      song_name.innerHTML = "映画『天空の城ラピュタ』：<br>innocent 系MHz";
+      song_shuoming.innerHTML = "为你推荐与 久石譲 - 映画『天空の城ラピュタ』：<br>innocent 相似的歌曲";
+      yuan5.style.backgroundColor = "#9DD6C5";
+      yuan1.style.backgroundColor = "#D4E1DC";
+      yuan3.style.backgroundColor = "#D4E1DC";
+      yuan4.style.backgroundColor = "#D4E1DC";
+      yuan2.style.backgroundColor = "#D4E1DC";
+      yuan6.style.backgroundColor = "#D4E1DC";
+  }
+  yuan6.onmouseover = function(){
+      my_tupian.src = "image/cute.png";
+      song_name.innerHTML = "SK-II 最初的梦MHz";
+      song_shuoming.innerHTML = "热门歌曲：夜空中最亮的星 / 最初的梦想（Cover 范玮<br>琪） / 一万次悲伤50首歌曲";
+      yuan6.style.backgroundColor = "#9DD6C5";
+      yuan1.style.backgroundColor = "#D4E1DC";
+      yuan3.style.backgroundColor = "#D4E1DC";
+      yuan4.style.backgroundColor = "#D4E1DC";
+      yuan5.style.backgroundColor = "#D4E1DC";
+      yuan2.style.backgroundColor = "#D4E1DC";
+  }
 
 
 
---
+++ 2.js
@@ -0,0 +1,14 @@
+(function () {
+  var div2 = document.getElementsByClassName("div2")[0];
+  var div1 = document.getElementsByClassName("div1")[0];
+  div1.onclick = function () {
+    div2.style.display = "block";
+
+  }
+ 
+
+
+
+
+
+ }(window))
